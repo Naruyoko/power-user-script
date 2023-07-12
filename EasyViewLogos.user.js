46ee124ef671c3c726df1582eea39ec7d5e11457
Naruyoko <naru519372@gmail.com>
2023-07-12 11:27:05 -0500

    Add my custom user scripts

---
+++ EasyViewLogos.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name     EasyView Logos
+// @version  1
+// @grant    none
+// @include  https://hunger.openfoodfacts.org/logos
+// @include  https://hunger.openfoodfacts.org/logos?*
+// @include  https://hunger.openfoodfacts.org/logos/deep-search*
+// @include  https://hunger.openfoodfacts.org/logos/deep-search?*
+// ==/UserScript==
+
+var css =
+    "div.css-1i1je6h {background-image:linear-gradient(rgba(255, 255, 255, 0.05), rgba(45, 206, 0, 0.6))}"+
+    "div.css-8a258q {background-image:linear-gradient(rgba(255, 255, 255, 0.05), rgba(181, 187, 1, 0.38))}"+
+    "div.css-1edw1j9 {opacity:0.3}",
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+head.appendChild(style);
+
+style.type = 'text/css';
+if (style.styleSheet){
+  // This is required for IE8 and below.
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
---
+++ NutritionScalar.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name     Nutrition Scalar
+// @version  1
+// @grant    none
+// @include  https://world.openfoodfacts.org/cgi/product.pl?type=edit&code=*
+// ==/UserScript==
+
+function addScaleButton(hsel,sel){
+  function scaleValues(){
+    var multiplier=+eval(prompt("Enter the multiplier",1));
+    if (multiplier) document.querySelectorAll(sel).forEach(e=>e.value&&(e.value=e.value.replace(/\d*\.\d*|\d+/,v=>+(v*multiplier).toPrecision(7))));
+  }
+  var e=document.createElement("button");
+  e.textContent="×";
+  e.onclick=e=>e.preventDefault()+scaleValues();
+  e.style.padding="0 10px";
+  var he=document.querySelector(hsel);
+  he.appendChild(document.createElement("br"));
+  he.appendChild(e);
+}
+
+addScaleButton("th.nutriment_col",".nutriment_value_as_sold");
+addScaleButton("th.nutriment_col_prepared",".nutriment_value_prepared");
