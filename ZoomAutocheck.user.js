c42415267c652a02e2ccf81361d4fff466ed0536
Naruyoko <naru519372@gmail.com>
2026-09-01 07:30:06 +0900

    Fix

--- ZoomAutocheck.user.js
+++ ZoomAutocheck.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name      Open Food Facts Zoom Autocheck
-// @version   2026-08-31
+// @version   2026-09-01
 // @author    Naruyoko
 // @include   https://*.openfoodfacts.org/cgi/product.pl*
 // @icon      data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -8,22 +8,19 @@
 // @updateURL https://github.com/Naruyoko/power-user-script/raw/refs/heads/Naruyoko-custom/ZoomAutocheck.user.js
 // ==/UserScript==
 
-window.addEventListener("load",function (){
-  var s=new WeakSet();
-  new MutationObserver(function (ms){ms.forEach(function (m){m.addedNodes.forEach(function (n){
-    if (n.nodeType!=Node.ELEMENT_NODE) return;
-    var c=n.closest(".cropbox");
-    if (!c) return;
-    var i=c.querySelector("input[type=checkbox]");
-    if (!i){
-      s.delete(c);
-      return;
-    }
-    if (s.has(c)) return;
-    requestAnimationFrame(function (){
+window.addEventListener("load",()=>{
+  let f=false;
+  new MutationObserver(ms=>{
+    f||=ms.some(m=>m.addedNodes.values().some(
+      n=>n.nodeType==Node.ELEMENT_NODE&&n.querySelector("input[type=checkbox]")&&n.closest(".cropbox")
+    ));
+    f&&requestAnimationFrame(()=>{
+      if (!f) return;
+      f=false;
+      let i=document.querySelector(".cropbox input[type=checkbox]");
+      if (!i) return;
       i.checked=true;
       i.dispatchEvent(new Event("change"));
     });
-    s.add(c);
-  });});}).observe(document.body,{childList:true,subtree:true});
+  }).observe(document.body,{childList:true,subtree:true});
 });
